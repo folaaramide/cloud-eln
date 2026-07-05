7af29b521ecedd1f92f54f590225ee62f20055e6
folaaramide <afolabiaramide@gmail.com>
2026-07-05 17:43:54 +0100

    Add reusable base template with Jinja inheritance

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from config import Config
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_object(Config)
 
     @app.route("/")
     def home():

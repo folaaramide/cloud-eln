2cb35cf3c80a5e7740b01a0a89a283799d90bb7d
folaaramide <afolabiaramide@gmail.com>
2026-07-05 18:24:18 +0100

    Configure SQLAlchemy and Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 from config import Config
 
+from app.extensions import db
+from app.extensions import migrate
+
 from app.routes.home import home_bp
 from app.routes.dashboard import dashboard_bp
 from app.routes.experiments import experiments_bp
@@ -14,6 +17,10 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_object(Config)
+
+    db.init_app(app)
+
+    migrate.init_app(app, db)
 
     app.register_blueprint(home_bp)
     app.register_blueprint(dashboard_bp)
--- config.py
+++ config.py
@@ -2,4 +2,12 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "development-secret-key")
+
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "development-secret-key"
+    )
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///eln.db"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

43653ce4c879c3bc0cbb067c704238b3dd896210
folaaramide <afolabiaramide@gmail.com>
2026-07-05 17:10:45 +0100

    Create initial Flask application using application factory

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "<h1>Welcome to the Cloud ELN</h1>"
+
+    return app
